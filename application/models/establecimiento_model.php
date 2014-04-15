8c21be5473a2cdcdc419be180237d51c364a35dc
Alejandro Torres <jowyto@gmail.com>
2014-04-15 14:55:42 -0300

    fix para que no queden establecimientos bajo el sidebar

--- application/models/establecimiento_model.php
+++ application/models/establecimiento_model.php
@@ -12,10 +12,16 @@ Class Establecimiento_model extends CI_Model{
 	//   }
 	// }
 
-	public function getByBounds($longMin = 0.0, $longMax = 0.0, $latMin = 0.0, $latMax = 0.0, $filtros = null, $order = null, $page = 0, $per_page = 10){
+	public function getByBounds($longMin = 0.0, $longMax = 0.0, $latMin = 0.0, $latMax = 0.0, $ratio_sidebar = null, $filtros = null, $order = null, $page = 0, $per_page = 10){
 		//$longDiff = ($longMax - $longMin) / 20.0;
 		//$longMin += $longDiff;
 		//$longMax -= $longDiff;
+
+		if($ratio_sidebar){
+			$longDiff = ($longMax - $longMin) * $ratio_sidebar;
+			$longMin += $longDiff;
+			//$longMax -= $longDiff;			
+		}
 
 		$latDiff = ($latMax - $latMin) / 20.0;
 		$latMin += $latDiff;
